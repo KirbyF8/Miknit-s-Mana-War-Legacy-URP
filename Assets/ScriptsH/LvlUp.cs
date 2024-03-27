e63387b223ec4e91fcf2114281459508a1e41f9b
Héctor <hff150305@gmail.com>
2024-03-27 11:55:57 +0100

    Limpieza1

--- Assets/ScriptsH/LvlUp.cs
+++ Assets/ScriptsH/LvlUp.cs
@@ -13,33 +13,28 @@ public class LvlUp : MonoBehaviour
     private void Start()
     {
         characters = GetComponent<Character>();
-        //character = FindAnyObjectByType<Character>();
+        
 
         //Debug.Log(characters.HP);
         
     }
 
+    // TODO: Acortar esto
     public void LevelUpStat(int hp, int hp_max, int hpScale, int mp, int mp_max, int mpScale,
     int strenght, int strenghtScale, int magic, int magicScale,
     int defense, int defenseScale, int resistance, int resistanceScale,
     int speed, int speedScale, int dexterity, int dexterityScale,
     int luck, int luckScale, int movement)
     {
-        //Debug.Log(characters.HP);
-        //Debug.Log(hp);
-        //int[] statsToLevelUp; 
+        
+        //?      Intentar llenar la array sin tener que traelo de la funcion     int[] statsToLevelUp; 
         
         statsToLevelUp = new int[] {
-        hp, hp_max, strenght,
+        hp_max, mp_max, strenght,
         magic, defense, resistance,
         speed, dexterity, luck
         };
-        //statsToLevelUp = new int[8];
-
-
-        //statsToLevelUp[0] = characters.HP;
-        //statsToLevelUp[1] = character.HP_max;
-        //statsToLevelUp[2] = character.Strenght;
+       
 
         int[] scales = {
         hpScale, mpScale, strenghtScale,
@@ -57,8 +52,9 @@ public class LvlUp : MonoBehaviour
             if( randomNumber < scales[i])
             {
                 statsToLevelUp[i]++;
-                //Debug.Log(statsToLevelUp[i]);
+               
             }
+            //? WHY?
             Debug.Log(statsToLevelUp[i]);
         }
 
