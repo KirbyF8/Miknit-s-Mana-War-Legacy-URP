1d2da6f5cb5d6a9caa6186bc296ad3c6c85bd06a
Cristian067 <cristian.n.c005@gmail.com>
2024-04-09 14:54:01 +0200

    904C

--- Assets/ScriptsC/Declaraciones_Persistencia de datos.cs
+++ Assets/ScriptsC/Declaraciones_Persistencia de datos.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Declaraciones_Persistenciadedatos : MonoBehaviour
 {
+
+    private string path =  "/save.json";
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(Application.persistentDataPath);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("i"))
+        {
+            save();
+        };
     }
 
     private void cosas()
     {
-        //PlayerPrefs.S
+        
+    }
+
+
+    public void save()
+    {
+
+        Save save = new Save
+        {
+            Id = 0,
+            Name = "Jaime",
+            Description = "jaime"
+        };
+
+
+        string jsonContent = JsonUtility.ToJson(save);
+        Debug.Log(jsonContent);
+        File.WriteAllText(Application.persistentDataPath + path, jsonContent);
+
     }
 
 
---
+++ Assets/ScriptsC/Save.cs
@@ -0,0 +1,16 @@
+
+
+public class Save 
+{
+
+
+    public int Id;
+    public string Name;
+    public string Description;
+
+        
+  
+
+
+    
+}
