fd344980e78cbb7107d53536d8bfbc9fd20c7b15
Héctor <hff150305@gmail.com>
2024-05-27 18:54:45 +0200

    27/05/24.2

--- Assets/ScriptsH/VisualBattleV2 Esta vez es personal la venganza de Héctor, ahora con Cristian Parte 2.cs
+++ Assets/ScriptsH/VisualBattleV2 Esta vez es personal la venganza de Héctor, ahora con Cristian Parte 2.cs
@@ -29,12 +29,12 @@ public class VisualBattleV2 : MonoBehaviour
     private string atackerNameConst;
     private string defenderNameConst;
 
-    private bool starded = false;
+    private bool started = false;
 
     private IEnumerator coroutine;
 
-    private int auxDos = 1;
-    private int auxTres = 1;
+    private int actualAttacks = 0;
+    
 
     private bool isBattleOver;
 
@@ -43,6 +43,18 @@ public class VisualBattleV2 : MonoBehaviour
     private ParticleSystem atackerParticles;
     private ParticleSystem defenderParticles;
 
+    private bool Crited = false;
+
+    
+
+    private int aDMGlocal;
+    private int dDMGlocal;
+    private int aHITLocal;
+    private int dHITLocal;
+    private int aCRITLocal;
+    private int dCRITLocal;
+    private int aATKsLocal;
+    private int dATKsLocal;
    
  
     private void Start()
@@ -79,11 +91,11 @@ public class VisualBattleV2 : MonoBehaviour
        //callAttack(attacker, defender, aCrit, aDMG, aHit, aATKs);
     }
 
-    public void callAttack(Character atacker, Character defender, int aCrit, int aDMG, int aHit, int aATKs )
+    public void callAttack(Character atacker, Character defender)
     {
         //! Settear la corrutina y llamarla
 
-        coroutine = Attack(atacker,defender, aCrit, aDMG,aHit,aATKs);
+        coroutine = Attack(atacker,defender);
 
             StartCoroutine(coroutine);
         
@@ -91,86 +103,288 @@ public class VisualBattleV2 : MonoBehaviour
 
     }
 
-    private IEnumerator Attack(Character atacker, Character defender, int aCrit, int aDMG, int aHit, int aATKs)
+    private IEnumerator Attack(Character atacker, Character defender)
     {
-        
-       
+
+
 
         int aux = Random.Range(0, 101);
 
-        coroutine = ADamage(atacker, defender, aDMG, aATKs);
-
-        if (atacker.hp <= 0 || defender.hp <= 0)
+        coroutine = ADamage(atacker, defender);
+        if (!defenderTurn)
         {
-            //? Dejenlo ya esta muerto
-           yield return null;
-        }
-        else
-        {
-            if (aux <= aHit)
+            if (atacker.hp <= 0 || defender.hp <= 0)
             {
-                if (aDMG <= 0)
-                {
-                    //! No Damage
-                    yield return null;
-                }
-                else
-                {
-                    aux = Random.Range(0, 101);
-                    if (aux < aCrit)
-                    {
-                        StartCoroutine(coroutine);
-                    }
-                    else
-                    {
-                        StartCoroutine(coroutine);
-                    }
-
-                }
+                //? Dejenlo ya esta muerto
+                yield return null;
             }
             else
             {
-                //! miss
-                StartCoroutine(coroutine);
+                if (aux <= aHITLocal)
+                {
+                    if (aDMGlocal <= 0)
+                    {
+                        //! No Damage
+                        yield return null;
+                    }
+                    else
+                    {
+                        aux = Random.Range(0, 101);
+                        if (aux < aCRITLocal)
+                        {
+                            //! Crítico
+                            Crited = true;
+                            StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            Crited = false;
+                            StartCoroutine(coroutine);
+                        }
+
+                    }
+                }
+                else
+                {
+                    //! miss
+                    StartCoroutine(coroutine);
+                    yield return null;
+                }
+            }
+
+            yield return null;
+        }
+        else
+        {
+            if (atacker.hp <= 0 || defender.hp <= 0)
+            {
+                //? Dejenlo ya esta muerto
                 yield return null;
             }
-        }
+            else
+            {
+                if (aux <= dHITLocal)
+                {
+                    if (dDMGlocal <= 0)
+                    {
+                        //! No Damage
+                        yield return null;
+                    }
+                    else
+                    {
+                        aux = Random.Range(0, 101);
+                        if (aux < dCRITLocal)
+                        {
+                            //! Crítico
+                            Crited = true;
+                            StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            Crited = false;
+                            StartCoroutine(coroutine);
+                        }
 
-        yield return null;
+                    }
+                }
+                else
+                {
+                    //! miss
+                    StartCoroutine(coroutine);
+                    yield return null;
+                }
+            }
+
+            yield return null;
+        }
     }
 
 
-    private IEnumerator ADamage(Character atacker,Character defender,int aCrit, int aDMG, int aHit, int aATKs)
+    private IEnumerator ADamage(Character atacker,Character defender)
     {
-        
+        yield return new WaitForSeconds(1.5f);
         //Debug.Log(defenderParticles.name);
-        starded = true;
-        
-        //int aDMG = uiBattle.ReturnDamage();
+
+        started = true;
+       
       
         Animator atackerAnimator = atacker.gameObject.GetComponent<Animator>();
 
         atackerAnimator.SetBool("AttackV2", true);
-        Debug.Log(aDMG);
+        // Debug.Log(aDMG);
         yield return new WaitForSeconds(0.7f);
         atackerAnimator.SetBool("AttackV2", false);
-        defender.hp -= aDMG;
-        if (defenderTurn)
+
+        DealDamage(atacker, defender);
+        
+        if (!defenderTurn)
         {
-            atackerParticles.Play();
+            attacksA++;
+            
         }
-        else 
-        { 
-            defenderParticles.Play(); 
+        else
+        {
+            attacksD++;
         }
+        Debug.Log( attacksA + "--" + attacksD );
+        
+        actualAttacks++;
+
+        ParticleManager();
        
       
 
         yield return new WaitForSeconds(0.5f);
 
 
+        HealthBarUpdate(atacker, defender);
+        
+        started = false;
 
+        
+
+        
+
+
+        if (!defenderTurn)
+        {
+            if (attacksA < aATKsLocal && aATKsLocal > 2 && actualAttacks < 2)
+            {
+                
+                    // 2 Ataques
+                    coroutine = Attack(atacker, defender);
+                    StartCoroutine(coroutine);
+                
+            }
+            else
+            {
+                actualAttacks = 0;
+               
+                
+                if ( attacksD >= maxAttacksD && attacksA < aATKsLocal)
+                {
+                    // Ataca el mismo
+                    callAttack(atacker, defender);
+                }
+                else if ( attacksD >= maxAttacksD && attacksA >= aATKsLocal)
+                {
+                    yield return null;
+                }
+                else 
+                {
+                    // cambio de turno
+                    defenderTurn = !defenderTurn;
+                    coroutine = Attack(defender, atacker);
+                    StartCoroutine(coroutine);
+                }
+               
+               
+            }
+        }
+        else if (defenderTurn)
+        {
+            Debug.Log(actualAttacks);
+            if (attacksD < dATKsLocal && dATKsLocal > 2 && actualAttacks < 2)
+            {
+              
+                    // 2 Ataques
+                    coroutine = Attack(atacker, defender);
+                    StartCoroutine(coroutine);
+
+                
+
+            }
+            else
+            {
+                actualAttacks = 0;
+               
+               
+                if (attacksA >= maxAttacksA && attacksD < dATKsLocal)
+                {
+                    // Ataca el mismo
+                    callAttack(atacker, defender);
+                }
+                else if (attacksD >= maxAttacksD && attacksA >= aATKsLocal)
+                {
+                    yield return null;
+                }
+                else 
+                {
+                    // Cambio de Turno
+                    defenderTurn = !defenderTurn;
+                    coroutine = Attack(defender, atacker);
+                    StartCoroutine(coroutine);
+                }
+                
+            }
+        }
+        
         /*
+         if (auxDos <= aATKs - 1 && auxTres < 2 && attacksA < maxAttacksA)
+         {
+
+             auxDos++;
+             if (atacker.name == atackerNameConst)
+             {
+                 auxTres++;
+             }
+             attacksA++;
+                 coroutine = ADamage(atacker, defender, aCrit, aDMG, aHit, aATKs);
+             StartCoroutine(coroutine);
+         }
+         else if (!defenderTurn && attacksD < maxAttacksD)
+         {
+
+             auxDos = 1;
+             auxTres = 1;
+             defenderTurn = true;
+             battle.GetAllAttack();
+         }
+         if(defenderTurn && auxDos <= aATKs - 1 && attacksD < maxAttacksD)
+         {
+             yield return new WaitForSeconds(1.4f);
+             attacksD++;
+             auxDos = 1;
+             auxTres = 1;
+             defenderTurn = false;
+             battle.GetAllAttack();
+         }
+
+
+         if (aATKs > 2 && attacksA < aATKs)
+         {
+             coroutine = Attack(atacker, defender, aCrit, aDMG, aHit, aATKs);
+         }
+        */
+
+    }
+    public void GetAllAttackCosas(Character attacker,Character defender,int aCrit,int dCrit,int aDMG,int dDMG,int aATKs,int dATKs,int aHit,int dHit)
+    {
+        maxAttacksA = aATKs;
+        maxAttacksD = dATKs;
+
+        SaveBattle(attacker, defender, aCrit, dCrit, aDMG, dDMG, aATKs, dATKs, aHit, dHit);
+        Debug.Log(defenderTurn);
+       
+       
+        if (!defenderTurn)
+        {
+
+            callAttack(attacker, defender);
+
+        }
+        else
+        {
+            callAttack(defender, attacker);
+        }
+       
+
+
+    }
+
+    private void HealthBarUpdate(Character atacker, Character defender)
+    {
+
         if (atacker.name == atackerNameConst)
         {
             uiBattle.HealthUpdate(atacker, defender);
@@ -179,68 +393,79 @@ public class VisualBattleV2 : MonoBehaviour
         {
             uiBattle.HealthUpdate(defender, atacker);
         }
-        starded = false;
-      
-       
-        if (auxDos <= aATKs - 1 && auxTres < 2 && attacksA < maxAttacksA)
-        {
-            
-            auxDos++;
-            if (atacker.name == atackerNameConst)
-            {
-                auxTres++;
-            }
-            attacksA++;
-                coroutine = ADamage(atacker, defender, aDMG, aATKs);
-            StartCoroutine(coroutine);
-        }
-        else if (!defenderTurn && attacksD < maxAttacksD)
-        {
-            
-            auxDos = 1;
-            auxTres = 1;
-            defenderTurn = true;
-            battle.GetAllAttack();
-        }
-        if(defenderTurn && auxDos <= aATKs - 1 && attacksD < maxAttacksD)
-        {
-            yield return new WaitForSeconds(1.4f);
-            attacksD++;
-            auxDos = 1;
-            auxTres = 1;
-            defenderTurn = false;
-            battle.GetAllAttack();
-        }*/
-
-    
-        if (aATKs > 2 && attacksA < aATKs)
-        {
-            coroutine = Attack(atacker, defender, aCrit, aDMG, aHit, aATKs);
-        }
-
     }
-    public void GetAllAttackCosas(Character attacker,Character defender,int aCrit,int dCrit,int aDMG,int dDMG,int aATKs,int dATKs,int aHit,int dHit)
-    {
-        maxAttacksA = aATKs;
-        maxAttacksD = dATKs;
 
-        Debug.Log(defenderTurn);
-       
-       
-        if (!defenderTurn)
+    private void ParticleManager()
+    {
+
+    if (Crited)
         {
 
-            callAttack(attacker, defender, aCrit, aDMG, aHit, aATKs);
-
+            // Cambiar a Particulas De Crítico
+            if (defenderTurn)
+            {
+               
+                atackerParticles.Play();
+            }
+            else
+            {
+                defenderParticles.Play();
+            }
         }
         else
         {
-            callAttack(defender, attacker, dCrit, dDMG, dHit, dATKs);
+            if (defenderTurn)
+            {
+                atackerParticles.Play();
+            }
+            else
+            {
+                defenderParticles.Play();
+            }
         }
-       
-
-
+        
     }
 
+    private void SaveBattle(Character attacker, Character defender, int aCrit, int dCrit, int aDMG, int dDMG, int aATKs, int dATKs, int aHit, int dHit)
+    {
+
+
+      aDMGlocal = aDMG;
+      dDMGlocal = dDMG;
+      aHITLocal = aHit;
+      dHITLocal = dHit;
+      aCRITLocal = aCrit;
+      dCRITLocal = dCrit;
+      aATKsLocal = aATKs;
+      dATKsLocal = dATKs;
+
+}
+
+    private void DealDamage(Character atacker, Character defender)
+    {
+    if (!defenderTurn)
+        {
+            if (Crited)
+            {
+                defender.hp -= aDMGlocal * 2;
+            }
+            else
+            {
+                defender.hp -= aDMGlocal;
+            }
+        }
+        else
+        {
+            if (Crited)
+            {
+                defender.hp -= dDMGlocal * 2;
+            }
+            else
+            {
+                defender.hp -= dDMGlocal;
+            }
+        }
+       
+    }
 
 }
